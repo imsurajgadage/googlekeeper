73166834a8fdcff0646c2203e2edcdaab2a14136
suraj <surajgadage20@gmail.com>
2020-10-12 09:43:03 +0530

    First Version

---
+++ src/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Note(){
+
+  return(
+    <div className="note">
+      <h1>This is The Title</h1>
+      <p>This is a paragraph</p>
+    </div>
+  );
+}
+
+export default Note;
